82e2aca90e0ffa22caaa242e27538fc9a09fb6a3
pat-s <patrick.schratz@gmail.com>
2019-04-09 22:59:42 +0200

    use new macro

--- tic.R
+++ tic.R
@@ -1,17 +1,1 @@
-get_stage("install") %>%
-  add_step(step_install_deps())
-
-
-if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_step(step_build_bookdown("", envir = new.env())) %>%
-    add_step(step_push_deploy(path = "_book", branch = "gh-pages"))
-}
+do_build_bookdown(input = "")
